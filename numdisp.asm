647a06cc3f2b69b76f22f5cba651fcd4bca8e77d
Lucy Erickson <tflux3on@yahoo.com>
2019-12-31 01:13:02 -0800

    Create numdisp.asm

---
+++ numdisp.asm
@@ -0,0 +1,24 @@
+; Number Display in Skilldrick's 6502 Simulator
+; By Flux3on
+; Change Digits: 1st 2nd 3rd 4th 5th
+;                E   R   T   Y   U
+;                 D   F   G   H   J
+
+; Digit Locations (Addresses)
+define digit_one $00
+define digit_two $01
+define digit_three $02
+define digit_four $03
+define digit_five $04
+
+; ASCII locations on keyboard (Constants)
+define key_e $65
+define key_d $64
+define key_r $72
+define key_f $66
+define key_t $74
+define key_g $67
+define key_y $79
+define key_h $68
+define key_u $75
+define key_j $6A
